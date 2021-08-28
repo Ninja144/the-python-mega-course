8df6cde2646cb87fde71a78533951b48653e897f
Vadim <vadimkubs@gmail.com>
2021-08-28 15:55:55 +0200

    complete csv ...

--- pandas/csv_pandas.py
+++ pandas/csv_pandas.py
@@ -1,10 +1,25 @@
 import os
 import pandas
 
-print(os.listdir('./supermarkets'))
+print(*os.listdir('./supermarkets'))
+print()
 
 df1 = pandas.read_csv('./supermarkets/supermarkets.csv')
 print(df1)
+print()
 
 df2 = pandas.read_json('./supermarkets/supermarkets.json')
 print(df2)
+print()
+
+df3 = pandas.read_excel('./supermarkets/supermarkets.xlsx', sheet_name=0)
+print(df3)
+print()
+
+df4 = pandas.read_csv('./supermarkets/supermarkets-commas.txt')
+print(df4)
+print()
+
+df5 = pandas.read_csv('./supermarkets/supermarkets-semi-colons.txt', sep=';')
+print(df5)
+print()
