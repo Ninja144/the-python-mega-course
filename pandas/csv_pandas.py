dd1a70c231f9459e28f61536d5d47dbdca4ec38e
Vadim <vadimkubs@gmail.com>
2021-08-28 16:05:09 +0200

    add colums

--- pandas/csv_pandas.py
+++ pandas/csv_pandas.py
@@ -4,30 +4,34 @@ import pandas
 print(*os.listdir('./supermarkets'))
 print()
 
-df1 = pandas.read_csv('./supermarkets/supermarkets.csv')
-print(df1)
-print()
-
-df2 = pandas.read_json('./supermarkets/supermarkets.json')
-print(df2)
-print()
-
-df3 = pandas.read_excel('./supermarkets/supermarkets.xlsx', sheet_name=0)
-print(df3)
-print()
-
-df4 = pandas.read_csv('./supermarkets/supermarkets-commas.txt')
-print(df4)
-print()
-
-df5 = pandas.read_csv('./supermarkets/supermarkets-semi-colons.txt', sep=';')
-print(df5)
-print()
+# df1 = pandas.read_csv('./supermarkets/supermarkets.csv')
+# print(df1)
+# print()
+#
+# df2 = pandas.read_json('./supermarkets/supermarkets.json')
+# print(df2)
+# print()
+#
+# df3 = pandas.read_excel('./supermarkets/supermarkets.xlsx', sheet_name=0)
+# print(df3)
+# print()
+#
+# df4 = pandas.read_csv('./supermarkets/supermarkets-commas.txt')
+# print(df4)
+# print()
+#
+# df5 = pandas.read_csv('./supermarkets/supermarkets-semi-colons.txt', sep=';')
+# print(df5)
+# print()
 
 df6 = pandas.read_csv('./supermarkets/supermarkets-semi-colons.txt', sep=';', header=None)
 print(df6)
 print()
 
 df6.columns = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
+print(df6)
+print()
+
+df6.set_index("one", inplace=True)
 print(df6)
 print()
