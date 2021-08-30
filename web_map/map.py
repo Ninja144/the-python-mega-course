1f21db5a17d905f7901c8845517d3756850d3202
unknown <vadimkubs@gmail.com>
2021-08-30 14:52:14 +0200

    web map

--- web_map/map.py
+++ web_map/map.py
@@ -1,4 +1,7 @@
 import folium
 
-map = folium.Map(location=[54.72105836758883, 20.493974315870876], zoom_start=30)
+map = folium.Map(location=[54.72105836758883, 20.493974315870876], zoom_start=20, tiles='Stamen Terrain')
+
+map.add_child(folium.Marker(location=[54.7, 20.4], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 map.save("Map1.html")
