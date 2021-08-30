62193b9ac7e4df9ee15c36e747df4807703d2ce0
unknown <vadimkubs@gmail.com>
2021-08-30 16:40:49 +0200

    web map

--- web_map/map.py
+++ web_map/map.py
@@ -2,17 +2,16 @@ import folium
 import pandas
 
 data = pandas.read_csv('Volcanoes.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+elev = list(data['ELEV'])
 
-lat = list(data['LON'])
-lon = list(data['LAT'])
-
-map = folium.Map(location=[54.72109254419616, 20.494004804299397], zoom_start=15, tiles='Stamen Terrain')
+map = folium.Map(location=[37.95182453166775, -114.50700991660084], zoom_start=5, tiles='Stamen Terrain')
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[54.72109254419616, 20.494004804299397],[54.719608760713655, 20.492782895612876]]:
-    fg.add_child(folium.Marker(location=coordinates, popup='Шарага', icon=folium.Icon(color='red')))
-    # fg.add_child(folium.Marker(location=[54.719608760713655, 20.492782895612876], popup='Драмтеатр', icon=folium.Icon(color='pink')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
