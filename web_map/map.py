c40d020220974048c8447faa35cd5e1b34b14aeb
unknown <vadimkubs@gmail.com>
2021-08-30 16:56:35 +0200

    web map

--- web_map/map.py
+++ web_map/map.py
@@ -6,12 +6,22 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map = folium.Map(location=[37.95182453166775, -114.50700991660084], zoom_start=5, tiles='Stamen Terrain')
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
