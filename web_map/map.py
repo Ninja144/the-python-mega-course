249f96b5ef28319baf881b06a368d3e94229e4c7
Vadim <vadimkubs@gmail.com>
2021-08-30 22:06:06 +0200

    web map

--- web_map/map.py
+++ web_map/map.py
@@ -23,6 +23,7 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-    map.add_child(fg)
+fg.add_child(folium.GeoJson())
 
-    map.save("Map1.html")
+map.add_child(fg)
+map.save("Map1.html")
