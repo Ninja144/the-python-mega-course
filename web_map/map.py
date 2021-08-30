edf9a2c545756452c7df47186c8d46a2a8345f07
unknown <vadimkubs@gmail.com>
2021-08-30 14:41:18 +0200

    web map

--- web_map/map.py
+++ web_map/map.py
@@ -1,4 +1,4 @@
 import folium
 
-map = folium.Map(location=[54.72105836758883, 20.493974315870876])
+map = folium.Map(location=[54.72105836758883, 20.493974315870876], zoom_start=30)
 map.save("Map1.html")
