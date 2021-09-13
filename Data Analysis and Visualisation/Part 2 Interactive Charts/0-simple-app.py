5e065d034e8535cc2a0d76b8a00d38d45bcc0254
Vadim <vadimkubs@gmail.com>
2021-09-13 22:36:41 +0200

    data 2

--- Data Analysis and Visualisation/Part 2 Interactive Charts/0-simple-app.py
+++ Data Analysis and Visualisation/Part 2 Interactive Charts/0-simple-app.py
@@ -3,7 +3,7 @@ import justpy as jp
 
 def app():
     wp = jp.QuasarPage
-    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h1")
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h1 text-center q-pa-md")
     p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
     return wp
 
---
+++ Data Analysis and Visualisation/Part 2 Interactive Charts/2-simple-app.py
@@ -0,0 +1,68 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false
+    },
+    title: {
+        text: 'Average Rating by Month'
+    },
+    subtitle: {
+        text: 'According to the Course Reviews Dataset'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Month'
+        },
+        labels: {
+            format: '{value} km'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Rating'
+        },
+        labels: {
+            format: '{value}°'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}°C'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+
+
+jp.justpy(chart_def)
